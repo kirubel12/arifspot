74820b44f6f78b86e9bf4e21c72bc1306a6a8e6f
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-05 09:52:04 +0300

    feat: define fillable properties in Offer model and update offers migration

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "name",
+        "description",
+        "icon",
+    ];
 }
