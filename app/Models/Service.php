7a9c65b6df183eed0bd24c028b1aa0b28ae6b738
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-05 09:47:34 +0300

    feat: add fillable properties to Service model and update services migration

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'image',
+        'has_price',
+
+    ];
 }
