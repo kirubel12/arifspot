be205cbc769bc370e014574dfca4efb4cfc8da5e
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-05 10:28:09 +0300

    feat: add fillable properties to Subcity model and update migration for unique name

--- app/Models/Subcity.php
+++ app/Models/Subcity.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subcity extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+
+    public function vendors()
+    {
+        return $this->hasMany(Vendor::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
