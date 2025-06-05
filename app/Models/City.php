75e6ec165fd571efd7dec355f35dcbbd43562005
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-05 09:55:26 +0300

    feat: define fillable properties in City model and update cities migration

--- app/Models/City.php
+++ app/Models/City.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+        'image',
+        'latitude',
+        'longitude',
+        'priority',
+    ];
+
+
 }
--- database/migrations/2025_06_05_062033_create_cities_table.php
+++ database/migrations/2025_06_05_062033_create_cities_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('image')->nullable();
+            $table->enum('priority', ['1', '2', '3', '4', '5'])->default('3');
             $table->timestamps();
         });
     }
