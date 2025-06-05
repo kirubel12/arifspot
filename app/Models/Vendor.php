5b77341cb75943d4e3b26964e11e1cc5c14b6bc5
kirubel12 <kirubelhabte1991@gmail.com>
2025-06-05 10:03:53 +0300

    feat: implement Vendor and Subcity models with migrations

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Vendor extends Model
 {
-    //
+    protected $fillable = [
+        "user_id",
+        "name",
+        "description",
+        "phone_number",
+        "images",
+        "rating",
+        "city_id",
+        "category_id",
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function city()
+    {
+        return $this->belongsTo(City::class);
+    }
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2025_06_05_062049_create_vendors_table.php
+++ database/migrations/2025_06_05_062049_create_vendors_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,15 @@ return new class extends Migration
     {
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('images')->nullable();
+            $table->enum('rating', ['1', '2', '3', '4', '5'])->default('3');
+            $table->foreignId('city_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+
             $table->timestamps();
         });
     }
